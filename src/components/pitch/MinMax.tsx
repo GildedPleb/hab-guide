33c57e8e13e6ccba041041a6a8e042831f49d276
GildedPleb <gildedpleb@protonmail.com>
2023-01-05 14:36:29 -0800

    Test autoplay

--- src/components/pitch/MinMax.tsx
+++ src/components/pitch/MinMax.tsx
@@ -295,7 +295,7 @@ const History = ({ pos }: { pos: number | null }) => {
       <FifthBlock>
         <ScrollClipPath start={0.925} end={1} from={50} to={0} pos={pos}>
           <VideoContainer>
-            <Video autoPlay loop muted playsinline preload="none">
+            <Video autoPlay autoplay loop muted playsinline preload="none">
               <source src="blue-node.mp4" type="video/mp4" />
             </Video>
           </VideoContainer>
